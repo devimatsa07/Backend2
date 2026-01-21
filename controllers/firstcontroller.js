6a46f41a9a0d6970a30de7a7f703fd8c7bd8bd08
devimatsa07 <matsadevi07@gmail.com>
2026-01-21 11:57:08 +0530

    commit

--- controllers/firstcontroller.js
+++ controllers/firstcontroller.js
@@ -66,7 +66,8 @@ const UploadFile = (req, res) => {
         if (!req.files || req.files.length === 0) {
             return res.status(400).json({ message: 'No files uploaded' });
         }
-        res.status(200).json({ message: 'Files uploaded successfully', files: req.files });
+        const filenames = req.files.map(file => file.filename);
+        res.status(200).json({ message: 'Files uploaded successfully', filename: filenames[0], files: filenames });
     } catch (error) {
         res.status(500).json({ error: error.message });
     }
